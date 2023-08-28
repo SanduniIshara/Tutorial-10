26ccf03aa92d89015b3c275d5c0708717ad09a90
SanduniIshara <136650291+SanduniIshara@users.noreply.github.com>
2023-08-28 19:07:47 +0530

    Add files via upload

---
+++ Tuto10_01.scala
@@ -0,0 +1,22 @@
+object Tuto10_01 {
+
+  def main(args: Array[String]): Unit = {
+
+
+    def calculateAverage(temperatures: List[Int]): Double = {
+      val fahrenheitTemperatures = temperatures.map(celsius => (celsius * 9 / 5) + 32)
+      val sumOfTemperatures = fahrenheitTemperatures.reduce(_ + _)
+      val averageTemperature = sumOfTemperatures / temperatures.length
+
+      return averageTemperature
+    }
+
+    val inputTemperatures = List(0, 10, 20, 30)
+    val averageFahrenheit = calculateAverage(inputTemperatures)
+    println(s"Average Fahrenheit temperature: $averageFahrenheit")
+
+
+  }
+
+
+}
---
+++ Tuto10_02.scala
@@ -0,0 +1,20 @@
+object Tuto10_02 {
+
+
+  def main(args: Array[String]): Unit = {
+
+    def countLetterOccurrences(words: List[String]): Int = {
+      val wordLengths = words.map(_.length) // Using map to transform words into their lengths
+      val totalLetterCount = wordLengths.reduce(_ + _) // Using reduce to calculate the total count
+
+      return totalLetterCount
+    }
+
+    val inputWords = List("apple", "banana", "cherry", "date")
+    val totalOccurrences = countLetterOccurrences(inputWords)
+    println(s"Total count of letter occurrences: $totalOccurrences")
+
+
+  }
+
+}
